d29d548048d0ed4b4b12f4776e9bb56bfd7f1a30
Johannes Schickling <schickling.j@gmail.com>
2021-11-10 14:07:13 +0000

    better script

--- init.sh
+++ init.sh
@@ -1,14 +1,22 @@
-set -x 
+set -ex
 
 if [[ -z "${GH_REPO}" && -z "${GH_OWNER}" ]]; then
+  echo 'env var GH_REPO or GH_OWNER not provided'
+else
 	SUBDIR="${GH_SUBDIR:-.}"
+	# replaces \ with /
+	SUBDIR=${SUBDIR//\\/\/}
+
 	BRANCH="${GH_BRANCH:-main}"
+	
+	STRIP=$(echo $SUBDIR | tr -cd '/' | wc -c)
+	# add +1 for repo-folder and add +1 for last dir
+	STRIP=$(($STRIP + 2))
+
+	echo $STRIP
 
 	curl https://codeload.github.com/$GH_OWNER/$GH_REPO/tar.gz/$BRANCH | \
-		tar -xz --strip=2 $GH_REPO-$BRANCH/$SUBDIR
+		tar -xz --strip=$STRIP $GH_REPO-$BRANCH/$SUBDIR
 
-	ls
-  
-else
-  echo 'env var GH_REPO or GH_OWNER not provided'
+	ls $SUBDIR
 fi
